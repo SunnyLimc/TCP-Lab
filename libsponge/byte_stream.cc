9ae2b716329b5cc1e51a9dcbb9ac1eff6d0f1b9e
Limc <limc@limcode.cn>
2022-03-22 01:57:01 +0800

    feat: lab0 && lab1

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,85 +1,23 @@
 #include "byte_stream.hh"
 
-// Dummy implementation of a flow-controlled in-memory byte stream.
-
-// For Lab 0, please replace with a real implementation that passes the
-// automated checks run by `make check_lab0`.
-
-// You will need to add private members to the class declaration in `byte_stream.hh`
 using namespace std;
 
-ByteStream::ByteStream(const size_t cap) : capacity(cap) {}
+ByteStream::ByteStream(const size_t cap) : _capacity(cap) {}
 
 size_t ByteStream::write(const string &data) {
-    int ret;
-    if (stream.length() == capacity)
-        return 0;
-    if (data.length() + stream.length() <= capacity) {
-        stream += data;
-        ret = data.length();
-    } else {
-        int size_o = stream.length();
-        stream += data.substr(0, capacity - stream.length());
-        ret = capacity - size_o;
+    size_t length = min(data.size(), remaining_capacity());
+    if (data.length() != 0) {
+        _buffer.append(move(const_cast<string &>(data).substr(0, length)));
     }
-    pushed += ret;
-    return ret;
+    _write_count += length;
+    return length;
+    return 0;
 }
 
-//! \param[in] len bytes will be copied from the output side of the buffer
-string ByteStream::peek_output(const size_t len) const {
-    string ret;
-    if (len <= 0)
-        return "";
-    if (len >= stream.length()) {
-        ret = stream;
-    } else {
-        ret = stream.substr(0, len);
-    }
-    return ret;
-}
+string ByteStream::peek_output(const size_t len) const { return _buffer.concatenate().substr(0, len); }
 
-//! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    if (len <= 0)
-        return;
-    if (len >= stream.length()) {
-        stream = "";
-    } else {
-        stream = stream.substr(len);
-    }
-    popped += len;
+    size_t length = min(_buffer.size(), len);
+    _buffer.remove_prefix(length);
+    _read_count += length;
 }
-
-//! Read (i.e., copy and then pop) the next "len" bytes of the stream
-//! \param[in] len bytes will be popped and returned
-//! \returns a string
-std::string ByteStream::read(const size_t len) {
-    string ret;
-    if (len <= 0)
-        return "";
-    if (len >= stream.length()) {
-        ret = stream;
-        pop_output(len);
-    } else {
-        ret = stream.substr(0, len);
-        pop_output(len);
-    }
-    return ret;
-}
-
-void ByteStream::end_input() { inputEnded = true; }
-
-bool ByteStream::input_ended() const { return inputEnded; }
-
-size_t ByteStream::buffer_size() const { return stream.length(); }
-
-bool ByteStream::buffer_empty() const { return stream.length() == 0; }
-
-bool ByteStream::eof() const { return input_ended() && buffer_empty(); }
-
-size_t ByteStream::bytes_written() const { return pushed; }
-
-size_t ByteStream::bytes_read() const { return popped; }
-
-size_t ByteStream::remaining_capacity() const { return capacity - stream.length(); }
--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -7,20 +7,118 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
 
-//! \details This function accepts a substring (aka a segment) of bytes,
-//! possibly out-of-order, from the logical stream, and assembles any newly
-//! contiguous substrings and writes them into the output stream in order.
+// \details This function accepts a substring (aka a segment) of bytes,
+// possibly out-of-order, from the logical stream, and assembles any newly
+// contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (_eofed) {
+        return;
+    }
+    if (eof && not _eof) {
+        _eof = true;
+        _eof_index = index + data.length();
+    }
+
+    // states and judge
+    auto index_c = index;
+    auto length_c = data.length();
+    auto end_index = index_c + length_c - 1;
+    auto max_acindex = _first_index + _output.remaining_capacity();
+    if (end_index < _first_index || index_c >= max_acindex || length_c == 0) {
+        if (_eof && _first_index == _eof_index) {
+            _output.end_input();
+            _eofed = true;
+        }
+        return;
+    }
+    size_t offset = 0;
+
+    // preprocessing
+    if (index < _first_index) {
+        index_c = _first_index;
+        offset = _first_index - index;
+        length_c -= offset;
+    }
+    if (index_c + length_c > max_acindex) {
+        length_c = max_acindex - index_c;
+        end_index = index_c + length_c - 1;
+    }
+    //! c++ always return a shadow copy for retrun value optimize, so it can directly bind to && type or normal type
+    //! move(it) actually get it memory ownership from reference, let x = move(it), it will trigger a shadow copy, copy
+    //! the same memory to x (not the reference)
+    BufferList buf{move(const_cast<string &>(data).substr(offset, length_c))};
+    auto it = _unassembled.lower_bound(index_c);
+    bool merged = false;
+    bool finish = false;
+
+    //! be care when convert while to if. do not omit ANY conditions
+    if (it != _unassembled.cbegin() || (it != _unassembled.cend() && it->first == index_c)) {
+        //!! if a seg with a same start_index existed, one of the two will be discarded
+        //!! handle duplicate start_index problem
+        if (index_c != it->first) {
+            --it;  // if start_index is same, you do not need to check the previous index
+        }
+        //! plus 1 to solve the case that 0:"a" 1:"b" and combine it to "ab"
+        if (it->second.first + 1 >= index_c) {
+            if (it->second.first - index_c + 1 >= length_c)
+                // just discard if this candidate seg covered by the previous range
+                finish = true;
+            else {
+                auto olap_len = it->second.first - index_c + 1;  //! in the case of adjacent merge, the olap_len is 0
+                buf.remove_prefix(olap_len);
+                it->second.second.append(buf);
+                merged = true;
+                _unassembled_bytes += buf.size();
+                //!! need to update states after merge
+                it->second.first = end_index;
+            }
+        }
+    }
+
+    auto insert = it;
+    if (not finish) {
+        if (not merged) {
+            it = _unassembled.insert(it, make_pair(index_c, make_pair(end_index, buf)));
+            _unassembled_bytes += buf.size();
+        }
+        insert = it++;
+        while (it != _unassembled.cend()) {
+            if (insert->second.first >= it->second.first) {
+                _unassembled_bytes -= it->second.second.size();
+                it = _unassembled.erase(it);
+                //! el-if implicit: insert->first.second < it->first.second
+            } else if (insert->second.first + 1 >= it->first) {  //! solve the adjacent problem described above
+                auto olap_len = insert->second.first - it->first + 1;
+                _unassembled_bytes -= olap_len;
+                it->second.second.remove_prefix(olap_len);
+                insert->second.second.append(it->second.second);
+                //!! need to update states after merge
+                insert->second.first = it->second.first;
+                _unassembled.erase(it);
+                break;
+            } else {
+                break;
+            }
+            // do not need to ++it, it will automatically exectued with "it = _unassembled.erase(it);"
+        }
+    }
+    if (insert->first == _first_index) {
+        auto seg_len = insert->second.second.size();
+        _first_index += seg_len;
+        _unassembled_bytes -= seg_len;
+        _output.write(insert->second.second.concatenate());
+        _unassembled.erase(insert);
+    }
+    if (_eof && _first_index == _eof_index) {
+        _output.end_input();
+        _eofed = true;
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _unassembled_bytes == 0; }
